1e187ead3b831be11132a0272f73906c049d8d6a
Pedro Leinar <pedroleinar@hotmail.com>
2020-04-06 21:28:26 -0300

    finished

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.put('/repositories/:id', (req, res) => {
     title,
     url,
     techs,
-    likes: 0,
+    likes: repositories[repositoryIndex].likes,
   };
 
   repositories[repositoryIndex] = repository;
